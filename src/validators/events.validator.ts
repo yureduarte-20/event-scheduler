99101ec6b5f2eec4e27b2a32b7a2314a5eece608
Yure Samarone <yuresamarone34@gmail.com>
2022-07-21 23:32:15 -0300

    fix: validators

--- src/validators/events.validator.ts
+++ src/validators/events.validator.ts
@@ -4,15 +4,15 @@ export default {
     get:[ query('startsAt').isDate().optional() ],
     post:[
         body('name').isString().notEmpty({ ignore_whitespace:true }),
-        body('starts_at').isDate().notEmpty(),
-        body('ends_at').isDate().notEmpty(),
+        body('starts_at').isString().notEmpty(),
+        body('ends_at').isString().notEmpty(),
         body('room').optional(),
         body('place_id').isInt()
     ],
     patch:[
         body('name').isString().notEmpty({ ignore_whitespace:true }).optional(),
-        body('starts_at').isDate().notEmpty().optional(),
-        body('ends_at').isDate().notEmpty().optional(),
+        body('starts_at').isString().notEmpty().optional(),
+        body('ends_at').isString().notEmpty().optional(),
         body('room').optional().optional(),
         body('place_id').isInt().optional(),
     ],
